3f7e9e1b57fd5606d928561a4c250cdac48aeb8c
cookielolwow <marianamejiagallego@gmail.com>
2025-07-21 21:49:58 -0500

    Add conclusions for exercises 8 to 10 and implement exercises 9 and 10 in assembly language

--- Experiencia de aprendizaje 1 Arquitectura del computador/Reto/Conclusiones reto.md
+++ Experiencia de aprendizaje 1 Arquitectura del computador/Reto/Conclusiones reto.md
@@ -6,3 +6,15 @@ Adicionalmente, aprendi que el -1 en el simulador que estoy usando es 65535
 
 
 ## EJERCICIO 8 ##
+* En el ejercicio ocho tuve que responder unas preguntas del tema de variables.
+Entendi mejor que las variables se almacenan a partir de la dirección 16 en adelante, siguiendo el orden en que se utilizan.
+
+## EJERCICIO 9##
+* En este reto subio la dificultad del tema de las variables. Para optimizar el fragmento del ejercicio inicie la variable i y luego le incremente directamente el valor almacenado en esa direccion de la memoria  (i = i + 1).
+
+## EJERCICIO 10##
+* Para este ejercicio cargue el valor M de R0 en el registro  D, para multiplicarlo por 2 asigne en D la suma D + D. Luego fui a R1 y guarde el resultado en el.
+
+
+
+
---
+++ Experiencia de aprendizaje 1 Arquitectura del computador/Reto/Ejercicio 9.asm
@@ -0,0 +1,37 @@
+//9. Considera el siguiente programa:
+    
+
+    i = 1
+    sum = 0
+    sum = sum + i
+    i = i + 1
+
+    
+    La traducción a lenguaje ensamblador del programa anterior es:
+    
+    // i = 1
+    @i
+    M=1
+    // sum = 0
+    @sum
+    M=0
+    // sum = sum + i
+    @i
+    D=M
+    @sum
+    M=D+M
+    // i = i + 1
+    @i
+    D=M+1
+    @i
+    M=D
+    
+    //- ¿Qué hace este programa?
+    // Respuesta: Este programa inicializa la variable `i` en 1, la variable `sum`
+    // en 0, luego suma el valor de `i` a `sum` y finalmente incrementa `i` en 1.
+    //- ¿En qué parte de la memoria RAM está la variable `i` y `sum`? ¿Por qué en esas posiciones?
+    // Respuesta: La variable `i` está en la posición 16 y `sum` en la posición 17, ya que las variables se asignan a partir de la posición 16 en adelante.
+    //- Optimiza esta parte del código para que use solo dos instrucciones:
+    
+  @i
+   M=M+1
