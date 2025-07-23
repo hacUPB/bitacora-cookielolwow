56bdddf22165d8fa6b8a4da9f55f23adc5f26cc7
cookielolwow <marianamejiagallego@gmail.com>
2025-07-23 17:12:40 -0500

    Update EJERCICIO 16.asm

--- Experiencia de aprendizaje 1 Arquitectura del computador/Reto/EJERCICIO 16.asm
+++ Experiencia de aprendizaje 1 Arquitectura del computador/Reto/EJERCICIO 16.asm
@@ -6,9 +6,21 @@
     for (int j = 0; j < 10; j++) {
         sum = sum + arr[j];
     }
-    
+@10
+A=D
 @arr
+M=D
+D=M
+(loop)
+@j
+A=D+M
+
+
+
+
+
     //- ¿Qué hace este programa?
     //- ¿Cuál es la dirección base de arr en la memoria RAM?
     //- ¿Cuál es la dirección base de sum en la memoria RAM y por qué?
     //- ¿Cuál es la dirección base de j en la memoria RAM y por qué?
+
